27730125a017291b45c7f488812caf620cadce9a
iramidalura <dalura.eramae@gmail.com>
2024-01-04 17:22:31 +0800

    ERA

--- src/components/screens/Timer/TimerScreen.js
+++ src/components/screens/Timer/TimerScreen.js
@@ -1,10 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 
-export default function TimerScreen() {
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  const handleStartStop = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = () => {
+    setSeconds(0);
+    setIsActive(false);
+  };
+
   return (
-    <View>
-      <Text>TimerScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.timerText}>{seconds}s</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={isActive ? 'Pause' : 'Start'}
+          onPress={handleStartStop}
+          color={isActive ? '#FF6347' : '#008000'}
+        />
+        <Button title="Reset" onPress={handleReset} />
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timerText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+});
+
+export default Timer;
